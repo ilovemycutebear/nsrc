2660f2c8b8b6a7acdc8b343d59c1e96ca64d5e63
ilovemycutebear <lery.018.official@gmail.com>
2015-07-02 15:31:44 +0800

    added davao and puerto

---
+++ Puerto/1_ftp_list_puerto.php
@@ -0,0 +1,56 @@
+<?php
+	include('databaseconn.php');
+    set_time_limit(300);//for setting 
+    $path='/puerto';
+    $ftp_server='ftp.meteopilipinas.gov.ph';
+    $ftp_server_port="21";
+    $ftp_user_name='nsrc';
+    $ftp_user_pass='N$rc015';
+
+    // set up a connection to ftp server
+    $conn_id = ftp_connect($ftp_server, $ftp_server_port); 
+    // login with username and password
+    $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+
+    // check connection and login result
+    if ((!$conn_id) || (!$login_result)) { 
+        echo "Fail</br>";
+    } else {
+        //echo "Success</br>";
+        // enabling passive mode
+        ftp_pasv( $conn_id, true );
+        // get contents of the current directory
+        $contents = ftp_nlist($conn_id,$path);
+        // output $contents
+        foreach($contents as $key => $value)
+		{
+  			//echo $value."</br>";
+  			$res = ftp_size($conn_id, $value);
+  			//echo $res."bytes</br>";
+
+		  			
+
+		$sql1 = "SELECT * FROM tblftpfilenames where tblftpfile =  '".$value."'";
+			$result1 = mysqli_query($conn, $sql1);
+
+		if (mysqli_num_rows($result1) > 0) {
+   				 while($row = mysqli_fetch_assoc($result1)) {
+  			echo "file name: ".$row["tblftpfile"]." is existing. <br />";
+		}
+  }
+  else{
+	$sql = "INSERT INTO tblftpfilenames (RecNum, tblAreaID, tblAreaName, tblftpfile, tblftpfilesize,FileState)
+VALUES ('', '0004', '".$path."','".$value."','".$res."','0')";
+
+if (mysqli_query($conn, $sql)) {
+    echo $value." recorded successfully<br />";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+	}
+ }//for each
+} //first else
+mysqli_close($conn);
+ ftp_close($conn_id);
+
+    ?>
---
+++ Puerto/2_stor_puerto_file.php
@@ -0,0 +1,154 @@
+<?php
+ini_set('max_execution_time', 3000);
+include('databaseconn.php');
+$slrDate =0;
+$slrTime =0;
+$slrAvgUv =0;
+$slrMinUv =0;
+$slrMaxUv =0;
+$slrAvgUvTmp =0;
+$slrMinUvTmp =0;
+$slrMaxUvTmp =0;
+$slrAvgRad =0;
+$slrMinRad =0;
+$slrMaxRad =0;
+$slrBatt =0;
+$slrAvgSunshine =0;
+$cnt=0;
+$cnt2=0;
+
+$sql1 = "SELECT * FROM tblftpfilenames WHERE FileState=0 AND tblAreaID=0004";
+			$result1 = mysqli_query($conn, $sql1);
+
+		if (mysqli_num_rows($result1) > 0) {
+   				 while($row = mysqli_fetch_assoc($result1)) {
+  			//echo "file name: ".$row["tblftpfile"]." is being opened. <br />";
+
+$filename = 'ftp://nsrc:N$rc015@ftp.meteopilipinas.gov.ph'.$row["tblftpfile"];
+$handle = fopen($filename, "r");
+
+
+//$contents = fread($handle, filesize($filename));
+while(! feof($handle))
+  {
+  	$cnt=$cnt+1;
+   $parts = preg_split('/\s+/',fgets($handle));
+   if($cnt>=9){
+    foreach(array_values($parts) as $key => $value)
+		{
+   			
+
+   			//print_r(array_values($parts));
+			//echo $value."&nbsp";
+
+			$solarv = preg_split('/\s+/',$value);
+
+			foreach(array_values($solarv) as $data => $findata)
+			{
+				$cnt2=$cnt2+1;
+				//echo $findata."splitted value ".$cnt2."<br/>";
+
+											if($cnt2==1){
+												
+							$datef = explode('.',$findata);
+							$slrDate  = $datef[2]."-".$datef[1]."-".$datef[0]; //date, month , year
+
+
+							}
+										else if($cnt2==2){
+
+							$slrTime = $findata;		
+							}
+										else if($cnt2==3){
+
+							$slrAvgUv = $findata;		
+							}
+										else if($cnt2==4){
+
+							$slrMinUv = $findata;		
+							}
+										else if($cnt2==5){
+
+							$slrMaxUv = $findata;		
+							}
+										else if($cnt2==6){
+
+							$slrAvgUvTmp = $findata;		
+							}
+										else if($cnt2==7){
+
+							$slrMinUvTmp = $findata;		
+							}
+										else if($cnt2==8){
+
+							$slrMaxUvTmp = $findata;		
+							}
+										else if($cnt2==9){
+
+							$slrBatt = $findata;		
+							}
+										else if($cnt2==10){
+
+							$slrAvgRad = $findata;		
+							}
+										else if($cnt2==11){
+
+							$slrMinRad = $findata;		
+							}
+										else if($cnt2==12){
+
+							$slrMaxRad = $findata;		
+							}
+										else if($cnt2==13){
+
+							$slrAvgSunshine = $findata;		
+							}
+			}
+		}
+		$sql = "INSERT INTO tblftpfiledata (RecNum, tblAreaID, slrDate, slrTime, AvgUv, MinUv, MaxUv, AvgTmpUv, MinTmpUv, MaxTmpUv, BattVolt, AvgSolRad, MinSolRad, MaxSolRad, AvgSunshine) 
+													 	VALUES ('', '0002','".$slrDate."','".$slrTime."','".$slrAvgUv."','".$slrMinUv."','".$slrMaxUv."','".$slrAvgUvTmp."','".$slrMinUvTmp."','".$slrMaxUvTmp."','".$slrBatt."','".$slrAvgRad."','".$slrMinRad."','".$slrMaxRad."','".$slrAvgSunshine."')";
+
+							if (mysqli_query($conn, $sql)) {
+  					//	  echo $value." recorded successfully<br />";
+								} else {
+  							  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+										}
+		$cnt2=0;
+		$slrDate =0;
+$slrTime =0;
+$slrAvgUv =0;
+$slrMinUv =0;
+$slrMaxUv =0;
+$slrAvgUvTmp =0;
+$slrMinUvTmp =0;
+$slrMaxUvTmp =0;
+$slrAvgRad =0;
+$slrMinRad =0;
+$slrMaxRad =0;
+$slrBatt =0;
+$slrAvgSunshine =0;
+	}
+	//	echo "line: ".$cnt."<br/>";
+	
+  }
+ $cnt=0;
+
+fclose($handle);
+$sqla = "UPDATE tblftpfilenames SET FileState='1' WHERE tblftpfile='".$row["tblftpfile"]."'";
+
+			if (mysqli_query($conn, $sqla)) {
+			    echo "Record updated successfully";
+			} else {
+			    echo "Error updating record: " . mysqli_error($conn);
+					}
+	}
+}// bracket for if result is > 0
+else{
+
+
+	 echo "All records are updated.";
+
+
+}
+mysqli_close($conn);
+?>
